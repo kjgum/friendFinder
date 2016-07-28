b333dad70a5db6d9458fad75d66ef80dd63c8729
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-07-28 12:24:23 -0400

    node files, home.html

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ var PORT = process.env.PORT || 3000;
 
 // Body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // Static components
 app.use('/', express.static(__dirname + '/app/public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 // Routes
 app.use('/', htmlRoutes);
 app.use('/', apiRoutes);
 
 // Start listening on port
-app.listen(PORT, function(){
-  console.log('App listening on PORT ' + PORT);
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
 })
